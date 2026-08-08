b2c59eb5ebf49002236da939c6d819cc7ecb9853
cristalavrill <c.martinez1@lanuevaamerica.edu.co>
2026-08-08 16:51:17 -0500

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="home">
+      <h1>Paradise Nursery</h1>
+
+      <p>Tu tienda de plantas de interior</p>
+
+      <button>Comenzar</button>
+    </div>
+  );
+}
+
+export default App;
