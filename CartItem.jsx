608d7a646a28263fa69318482500229cf358eac6
cristalavrill <c.martinez1@lanuevaamerica.edu.co>
2026-08-10 20:25:19 -0500

    Refactor CartItem component with Redux hooks

--- CartItem.jsx
+++ CartItem.jsx
@@ -1,72 +1,119 @@
-import { useState } from "react";
-import { Provider, useSelector } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  increaseQuantity,
+  decreaseQuantity,
+  removeFromCart,
+} from "./CartSlice";
 
-import cartReducer from "./CartSlice";
-import ProductList from "./ProductList";
-import CartItem from "./CartItem";
-import AboutUs from "./src/components/AboutUs";
+function CartItem() {
+  const dispatch = useDispatch();
 
-const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-  },
-});
+  const cartItems = useSelector((state) => state.cart.items);
 
-function AppContent() {
-  const [page, setPage] = useState("home");
-
-  const totalItems = useSelector((state) =>
-    state.cart.items.reduce((total, item) => total + item.quantity, 0)
+  const total = cartItems.reduce(
+    (sum, item) => sum + item.price * item.quantity,
+    0
   );
 
-  if (page === "plants") {
-    return (
-      <div>
-        <button onClick={() => setPage("home")}>Inicio</button>
-        <button onClick={() => setPage("cart")}>
-          Carrito ({totalItems})
-        </button>
-
-        <ProductList />
-      </div>
-    );
-  }
-
-  if (page === "cart") {
-    return (
-      <div>
-        <button onClick={() => setPage("plants")}>
-          Continuar comprando
-        </button>
-
-        <CartItem />
-      </div>
-    );
-  }
+  const totalItems = cartItems.reduce(
+    (sum, item) => sum + item.quantity,
+    0
+  );
 
   return (
-    <div className="home">
-      <h1>Paradise Nursery</h1>
+    <div id="cart">
+      <nav>
+        <a href="/">Inicio</a>{" "}
+        <a href="#plants">Plantas</a>{" "}
+        <a href="#cart">🛒 Carrito ({totalItems})</a>
+      </nav>
 
-      <p>Tu tienda de plantas de interior</p>
+      <h1>Carrito de Compras</h1>
 
-      <button onClick={() => setPage("plants")}>
-        Comenzar
-      </button>
+      {cartItems.length === 0 ? (
+        <div>
+          <p>Tu carrito está vacío.</p>
 
-      <AboutUs />
+          <a href="#plants">
+            <button>Continuar comprando</button>
+          </a>
+        </div>
+      ) : (
+        <div>
+          {cartItems.map((item) => (
+            <div key={item.id}>
+              <img
+                src={item.image}
+                alt={item.name}
+                width="150"
+              />
+
+              <h2>{item.name}</h2>
+
+              <p>
+                Precio unitario: $
+                {item.price.toLocaleString()}
+              </p>
+
+              <p>
+                Cantidad: {item.quantity}
+              </p>
+
+              <p>
+                Total de esta planta: $
+                {(item.price * item.quantity).toLocaleString()}
+              </p>
+
+              <button
+                onClick={() =>
+                  dispatch(decreaseQuantity(item.id))
+                }
+                disabled={item.quantity === 1}
+              >
+                −
+              </button>
+
+              <span> {item.quantity} </span>
+
+              <button
+                onClick={() =>
+                  dispatch(increaseQuantity(item.id))
+                }
+              >
+                +
+              </button>
+
+              <button
+                onClick={() =>
+                  dispatch(removeFromCart(item.id))
+                }
+              >
+                Eliminar
+              </button>
+            </div>
+          ))}
+
+          <h2>
+            Total del carrito: $
+            {total.toLocaleString()}
+          </h2>
+
+          <button
+            onClick={() => alert("Próximamente")}
+          >
+            Pagar
+          </button>
+
+          <br />
+          <br />
+
+          <a href="#plants">
+            <button>Continuar comprando</button>
+          </a>
+        </div>
+      )}
     </div>
   );
 }
 
-function App() {
-  return (
-    <Provider store={store}>
-      <AppContent />
-    </Provider>
-  );
-}
-
-export default App;
+export default CartItem;
