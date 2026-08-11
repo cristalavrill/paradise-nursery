3f00d3d675b7f09751015624df6886be31527d6e
cristalavrill <c.martinez1@lanuevaamerica.edu.co>
2026-08-10 20:24:45 -0500

    Refactor ProductList component and add new plants

--- ProductList.jsx
+++ ProductList.jsx
@@ -7,72 +7,128 @@ const plants = [
     name: "Monstera",
     price: 45000,
     category: "Plantas de interior",
-    image:
-      "https://images.unsplash.com/photo-1614594975525-e45190c55d0b",
+    image: "https://images.unsplash.com/photo-1614594975525-e45190c55d0b",
   },
   {
     id: 2,
     name: "Pothos",
     price: 35000,
     category: "Plantas de interior",
-    image:
-      "https://images.unsplash.com/photo-1614594895304-fe7116ac3b68",
+    image: "https://images.unsplash.com/photo-1614594895304-fe7116ac3b68",
   },
   {
     id: 3,
     name: "Snake Plant",
     price: 40000,
     category: "Plantas de interior",
-    image:
-      "https://images.unsplash.com/photo-1593691509543-c55fb32e5cee",
+    image: "https://images.unsplash.com/photo-1593691509543-c55fb32e5cee",
   },
   {
     id: 4,
-    name: "Aloe Vera",
-    price: 30000,
-    category: "Suculentas",
-    image:
-      "https://images.unsplash.com/photo-1509423350716-97f9360b4e09",
+    name: "Rubber Plant",
+    price: 38000,
+    category: "Plantas de interior",
+    image: "https://images.unsplash.com/photo-1598880940080-ff9a29891b85",
   },
   {
     id: 5,
-    name: "Echeveria",
-    price: 25000,
-    category: "Suculentas",
-    image:
-      "https://images.unsplash.com/photo-1459411621453-7b03977f4bfc",
+    name: "Spider Plant",
+    price: 32000,
+    category: "Plantas de interior",
+    image: "https://images.unsplash.com/photo-1572688484438-313a6e50c333",
   },
   {
     id: 6,
-    name: "Jade Plant",
-    price: 28000,
-    category: "Suculentas",
-    image:
-      "https://images.unsplash.com/photo-1509423350716-97f9360b4e09",
+    name: "Peace Lily",
+    price: 42000,
+    category: "Plantas de interior",
+    image: "https://images.unsplash.com/photo-1593691509543-c55fb32e5cee",
   },
+
   {
     id: 7,
-    name: "Calathea",
-    price: 50000,
-    category: "Plantas tropicales",
-    image:
-      "https://images.unsplash.com/photo-1614594975525-e45190c55d0b",
+    name: "Aloe Vera",
+    price: 30000,
+    category: "Suculentas",
+    image: "https://images.unsplash.com/photo-1509423350716-97f9360b4e09",
   },
   {
     id: 8,
-    name: "Peace Lily",
-    price: 42000,
-    category: "Plantas tropicales",
-    image:
-      "https://images.unsplash.com/photo-1593691509543-c55fb32e5cee",
+    name: "Echeveria",
+    price: 25000,
+    category: "Suculentas",
+    image: "https://images.unsplash.com/photo-1459411621453-7b03977f4bfc",
   },
   {
     id: 9,
-    name: "Fiddle Leaf Fig",
+    name: "Jade Plant",
+    price: 28000,
+    category: "Suculentas",
+    image: "https://images.unsplash.com/photo-1509423350716-97f9360b4e09",
+  },
+  {
+    id: 10,
+    name: "Haworthia",
+    price: 27000,
+    category: "Suculentas",
+    image: "https://images.unsplash.com/photo-1536353284924-9220c268e4d0",
+  },
+  {
+    id: 11,
+    name: "Sedum",
+    price: 23000,
+    category: "Suculentas",
+    image: "https://images.unsplash.com/photo-1485955900006-10f4d324d411",
+  },
+  {
+    id: 12,
+    name: "String of Pearls",
+    price: 36000,
+    category: "Suculentas",
+    image: "https://images.unsplash.com/photo-1501004318641-b39e6451bec6",
+  },
+
+  {
+    id: 13,
+    name: "Calathea",
+    price: 50000,
+    category: "Plantas tropicales",
+    image: "https://images.unsplash.com/photo-1614594975525-e45190c55d0b",
+  },
+  {
+    id: 14,
+    name: "Bird of Paradise",
+    price: 60000,
+    category: "Plantas tropicales",
+    image: "https://images.unsplash.com/photo-1614594895304-fe7116ac3b68",
+  },
+  {
+    id: 15,
+    name: "Philodendron",
+    price: 48000,
+    category: "Plantas tropicales",
+    image: "https://images.unsplash.com/photo-1614594975525-e45190c55d0b",
+  },
+  {
+    id: 16,
+    name: "Alocasia",
     price: 55000,
     category: "Plantas tropicales",
-    image:
-      "https://images.unsplash.com/photo-1614594895304-fe7116ac3b68",
+    image: "https://images.unsplash.com/photo-1593691509543-c55fb32e5cee",
+  },
+  {
+    id: 17,
+    name: "Croton",
+    price: 44000,
+    category: "Plantas tropicales",
+    image: "https://images.unsplash.com/photo-1545239351-1141bd82e8a6",
+  },
+  {
+    id: 18,
+    name: "Dieffenbachia",
+    price: 46000,
+    category: "Plantas tropicales",
+    image: "https://images.unsplash.com/photo-1509423350716-97f9360b4e09",
   },
 ];
 
@@ -95,7 +151,7 @@ function ProductList() {
       <nav>
         <a href="/">Inicio</a>{" "}
         <a href="#plants">Plantas</a>{" "}
-        <a href="#cart">Carrito ({totalItems})</a>
+        <a href="#cart">🛒 Carrito ({totalItems})</a>
       </nav>
 
       <h1>Paradise Nursery</h1>
