95ba8b57fc9af86a6072cbf9b381dc467820caeb
cristalavrill <c.martinez1@lanuevaamerica.edu.co>
2026-08-12 17:42:12 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,62 +1,26 @@
 import { useState } from "react";
-import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-
-import cartReducer from "./CartSlice";
-import ProductList from "./ProductList";
-import CartItem from "./CartItem";
-import AboutUs from "./src/components/AboutUs";
 import "./App.css";
-
-const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-  },
-});
+import ProductList from "./ProductList";
 
 function App() {
   const [showProductList, setShowProductList] = useState(false);
-  const [showCart, setShowCart] = useState(false);
+
+  if (showProductList) {
+    return <ProductList />;
+  }
 
   return (
-    <Provider store={store}>
-      <div className="app-container">
+    <div className="home">
+      <h1>Paradise Nursery</h1>
 
-        {!showProductList && !showCart ? (
-          <div className="home">
-            <div className="home-overlay">
-              <h1>Paradise Nursery</h1>
+      <p>
+        Bienvenido a Paradise Nursery, tu tienda de plantas de interior.
+      </p>
 
-              <p>
-                Tu tienda de plantas de interior
-              </p>
-
-              <button
-                onClick={() => setShowProductList(true)}
-              >
-                Comenzar
-              </button>
-            </div>
-          </div>
-        ) : showCart ? (
-          <CartItem
-            onContinueShopping={() => {
-              setShowCart(false);
-              setShowProductList(true);
-            }}
-          />
-        ) : (
-          <>
-            <ProductList
-              onViewCart={() => setShowCart(true)}
-            />
-
-            <AboutUs />
-          </>
-        )}
-
-      </div>
-    </Provider>
+      <button onClick={() => setShowProductList(true)}>
+        Comenzar
+      </button>
+    </div>
   );
 }
 
