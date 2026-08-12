69d07a042f6c27f2fd1598576f8e6d6d2026c7f2
cristalavrill <c.martinez1@lanuevaamerica.edu.co>
2026-08-12 17:35:35 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,14 +1,62 @@
+import { useState } from "react";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+
+import cartReducer from "./CartSlice";
+import ProductList from "./ProductList";
+import CartItem from "./CartItem";
+import AboutUs from "./src/components/AboutUs";
 import "./App.css";
 
+const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
+});
+
 function App() {
+  const [showProductList, setShowProductList] = useState(false);
+  const [showCart, setShowCart] = useState(false);
+
   return (
-    <div className="home">
-      <h1>Paradise Nursery</h1>
+    <Provider store={store}>
+      <div className="app-container">
 
-      <p>Tu tienda de plantas de interior</p>
+        {!showProductList && !showCart ? (
+          <div className="home">
+            <div className="home-overlay">
+              <h1>Paradise Nursery</h1>
 
-      <button>Comenzar</button>
-    </div>
+              <p>
+                Tu tienda de plantas de interior
+              </p>
+
+              <button
+                onClick={() => setShowProductList(true)}
+              >
+                Comenzar
+              </button>
+            </div>
+          </div>
+        ) : showCart ? (
+          <CartItem
+            onContinueShopping={() => {
+              setShowCart(false);
+              setShowProductList(true);
+            }}
+          />
+        ) : (
+          <>
+            <ProductList
+              onViewCart={() => setShowCart(true)}
+            />
+
+            <AboutUs />
+          </>
+        )}
+
+      </div>
+    </Provider>
   );
 }
 
